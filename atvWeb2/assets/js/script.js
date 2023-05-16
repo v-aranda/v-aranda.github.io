65432d8635405d85d078e99a91974ab6ee0f7c19
V-aranda <viniciuzaranda@gmail.com>
2023-05-16 15:46:27 -0300

    pronto

--- atvWeb2/assets/js/script.js
+++ atvWeb2/assets/js/script.js
@@ -3,7 +3,9 @@ const buttonNoticia = document.querySelector('.buttonnoticia')
 const buttonProblemas = document.querySelector('.buttonproblema')
 
 
+
 $("#content").load( "../pages/home.html" )
 buttonInicio.addEventListener('click', function(){$("#content").load( "../pages/home.html" )});
 buttonNoticia.addEventListener('click', function(){$("#content").load( "../pages/noticias.html" )});
 buttonProblemas.addEventListener('click', function(){$("#content").load( "../pages/problemas.html" )});
+
