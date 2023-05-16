30de441145664dcb390bdc5f9fc633e7dcbfc25b
V-aranda <viniciuzaranda@gmail.com>
2023-05-15 22:39:41 -0300

    .

---
+++ ficha-jojo/src/js/script.js
@@ -0,0 +1,30 @@
+function onload(){
+
+
+    class Pessoa{
+        constructor(nome,trevas,poder,resistencia,velocidade,precisão,alcance,potencial){
+            this.infos = {
+                nome : nome
+            },
+            this.status ={
+                vigor : 10,
+            },
+            this.atributos = {
+                poder : poder,
+                resistencia : resistencia,
+                velocidade : velocidade,
+                precisão : precisão,
+                alcance : alcance,
+                potencial : potencial,}
+        }
+        aumenta
+    }
+
+
+ 
+    const p1 = new Pessoa('NOME',0,'D','D','D','D','D','D')
+   
+    
+
+}
+onload()
